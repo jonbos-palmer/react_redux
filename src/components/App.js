c4f206500536aeacf88dc27162cd1c57789ffa51
jonbos-palmer <jbos@thepalmergroup.com>
2020-06-17 10:00:31 -0500

    Create app container component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import AboutPage from "./about/AboutPage";
+import HomePage from "./home/HomePage";
+function App() {
+  return (
+    <div className="container-fluid">
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/about" component={AboutPage} />
+    </div>
+  );
+}
+export default App;
